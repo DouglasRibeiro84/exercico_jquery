23b9b208e7f01b79eaeecb15db4d69a4e7972c8e
doouglas <douglasribeiro084@gmail.com>
2024-06-24 15:33:50 -0300

    final commit

--- main.js
+++ main.js
@@ -2,4 +2,42 @@ $(document).ready(function(){
     $('header button').click(function(){
         $('form').slideDown()
     })
+
+    $('#minimizar').click(function(){
+        $('form').slideUp()
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault()
+
+        const tarefas = $('#tarefa').val()
+        const novaTarefa = $("<li></li>")
+        const pendente = $('status')
+        $(`<p class="clickable" id="meuTexto">${tarefas}</p>`).appendTo(novaTarefa)
+
+        $(novaTarefa).appendTo('ul')
+        $(novaTarefa).fadeIn(1000)
+        $('#tarefa').val('')
+    })
+
+    $('#listaTextos').on('click', 'li', function() {
+        if (!$(this).hasClass('clicked')) {
+            $(this).addClass('clicked');
+        }
+    });
+
+    $('#limpar').click(function() {
+        $('#listaTextos').empty();
+    });
+
+    $('.clickable').click(function() {
+        $(this).toggleClass('concluido');
+        const status = $('#status').text();
+        
+        if (status === 'Pendente') {
+            $('#status').text('Concluído');
+        }
+    });
+
+
 })
